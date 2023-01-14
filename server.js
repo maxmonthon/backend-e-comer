fa12c3539bae0f2317b175bf3c2cb22ec23def8d
maxmonthon <70101075+maxmonthon@users.noreply.github.com>
2023-01-14 13:52:30 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const { readdirSync } = require('fs')
+const connectDB = require('./config/db')
+
+require('dotenv').config();
+
+const app = express()
+connectDB()
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json({limit:'20mb'}))
+app.use(cors())
+
+// Route
+// #test
+app.get('/tea',(req,res)=>{
+    res.send("fake shop coming")
+})
+
+// READ
+ readdirSync('./routes')
+ .map((r)=> app.use('/api', require('./routes/'+r)))
+
+
+
+
+const port =process.env.PORT
+app.listen(port,()=>{
+    console.log('Server is running on port '+port)
+})
